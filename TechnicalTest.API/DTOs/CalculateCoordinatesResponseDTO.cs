0f79e056c996913dc7f28c4de60bce8e3835c511
Saurav Lakhmani <sonithlakhmani@gmail.com>
2022-06-30 17:42:18 +0100

    Modied RespsonseDTOs

--- TechnicalTest.API/DTOs/CalculateCoordinatesResponseDTO.cs
+++ TechnicalTest.API/DTOs/CalculateCoordinatesResponseDTO.cs
@@ -1,19 +1,32 @@
-﻿namespace TechnicalTest.API.DTOs
+﻿using TechnicalTest.Core.Models;
+
+namespace TechnicalTest.API.DTOs
 {
     public class CalculateCoordinatesResponseDTO
     {
         public List<Coordinate> Coordinates { get; set; }
+        public string GridValue { get; set; }
+        public string Message { get; set; }
+        public int StatusCode { get; set; }
 
-        public class Coordinate
+        //public class Coordinate
+        //{
+        //    public int X { get; set; }
+        //    public int Y { get; set; }
+
+        //    public Coordinate(int x, int y)
+        //    {
+        //        X = x;
+        //        Y = y;
+        //    }
+        //}
+
+        public CalculateCoordinatesResponseDTO(List<Coordinate> coordinateList, string gridValue)
         {
-            public int X { get; set; }
-            public int Y { get; set; }
-
-            public Coordinate(int x, int y)
-            {
-                X = x;
-                Y = y;
-            }
+            Coordinates = coordinateList;
+            GridValue = gridValue.ToUpper();
+            Message = "Success";
+            StatusCode = 200;
         }
     }
 }
